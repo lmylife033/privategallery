51e2525f69baf91c94b8a4d428264fce95d8e99f
lmylife033 <lmylife033@yahoo.co.jp>
2019-02-16 17:21:03 +0900

    first commit

---
+++ app/Http/Controllers/Admin/SignupController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use APP\Signup;
+
+class SignupController extends Controller
+{
+    //
+    public function add()
+    {
+    	return view('admin.signup.create');
+    }
+    public function create(Requst $request)
+ {
+ return redirect('admin/signup/create');
+ }
+
+  public function edit()
+  {
+      return view('admin.signup.edit');
+  }
+
+  public function update()
+  {
+      return redirect('admin/signup/edit');
+      }
+
+}
